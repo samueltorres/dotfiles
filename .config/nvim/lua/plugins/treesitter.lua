c2bca40b4fc02e2cf78192d9759ef8acbe402dfc
Samuel Torres <samuel.torres@form3.tech>
2024-12-19 12:57:41 +0000

    feat: Add some improvements to the nvim config
    Signed-off-by: Samuel Torres <samuel.torres@form3.tech>

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+return {
+  -- Highlight, edit, and navigate code
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    opts = {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'go',
+        'gomod',
+        'gosum',
+        'gotmpl',
+        'hcl',
+        'terraform',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+  },
+}
